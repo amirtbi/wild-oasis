1778b0edc000dd0db3d43bd66cff804d4f20e904
amirtbi <torabi46@gmail.com>
2024-09-20 15:05:46 +0330

    setup react query

--- src/Routing/AppRoutes.tsx
+++ src/Routing/AppRoutes.tsx
@@ -6,33 +6,42 @@ import { PrivateRoutes } from "./PrivateRoutes";
 import { AuthPage } from "../pages/AuthPage";
 import { Error404 } from "../features/Error/404";
 import GlobalStyles from "../styles/GlobalStyles";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    },
+  },
+});
 const AppRoutes = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(true);
   return (
     <>
-      <GlobalStyles />
-
-      <BrowserRouter basename="/">
-        <Routes>
-          <Route element={<App />}>
-            <Route path="error/*" element={<ErrorPage />} />
-            <Route path="logout" element={<div>Logout page</div>}></Route>
-            {isLoggedIn ? (
-              <>
-                <Route path="/*" element={<PrivateRoutes />} />
-                <Route index element={<Navigate to="/dashboard" />} />
-              </>
-            ) : (
-              <>
-                <Route path="auth/*" element={<AuthPage />} />
-                <Route path="*" element={<Navigate to="/auth" />} />
-              </>
-            )}
-            <Route path="*" element={<Error404 />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <GlobalStyles />
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route element={<App />}>
+              <Route path="error/*" element={<ErrorPage />} />
+              <Route path="logout" element={<div>Logout page</div>}></Route>
+              {isLoggedIn ? (
+                <>
+                  <Route path="/*" element={<PrivateRoutes />} />
+                  <Route index element={<Navigate to="/dashboard" />} />
+                </>
+              ) : (
+                <>
+                  <Route path="auth/*" element={<AuthPage />} />
+                  <Route path="*" element={<Navigate to="/auth" />} />
+                </>
+              )}
+              <Route path="*" element={<Error404 />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
     </>
   );
 };
